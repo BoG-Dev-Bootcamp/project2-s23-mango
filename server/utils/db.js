b563922120cb6bffa648413f14453bd517f2a71a
rachelyuh <heavenyuh@gmail.com>
2023-03-30 19:42:55 -0400

    animal edits

---
+++ server/utils/db.js
@@ -0,0 +1,23 @@
+
+import mongoose from "mongoose";
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
+
+mongoose.set('strictQuery', false);
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(`${process.env.DB_URL}`)
+        console.log("Sucessfully connected to the database!")
+    } catch (e) {
+        console.log("Failed to connect to the database :(")
+    }
+}
+
+const closeDB = async () => {
+    await mongoose.connection.close();
+    console.log("Connection Closed.");
+}
+export { connectDB, closeDB };

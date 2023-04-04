a56999e68abc49089d17132d97a24f5a769c20ff
rachelyuh <heavenyuh@gmail.com>
2023-04-04 18:10:39 -0400

    added createAnimal

--- server/mongodb/index.js
+++ server/mongodb/index.js
@@ -4,8 +4,8 @@ export default async function connectDB() {
     if (mongoose.connections[0].readyState) return;
 
     await mongoose
-        .connect(process.env.MONGO_DB, {
-            dbName: process.env.DB_URL,
+        .connect(process.env.DB_URL, {
+            dbName: process.env.DB_NAME,
         })
         .catch((e) => {
             console.error("Error connecting to database.");

560db5866f9dfd0e0b6977c37b3a9f553c3ec4a7
Sriteja Nara <sritejanara@gmail.com>
2023-04-06 19:27:27 -0400

    created get endpoint at api/admin/training to return all training logs

---
+++ server/mongodb/actions/createTrainingLog.js
@@ -0,0 +1,16 @@
+import connectDB from "../index.js"
+import Training from "../models/trainingLog"
+
+export default async function createLog(logData) {
+    await connectDB()
+    try {
+        console.log("INSIDE OF TRY")
+        console.log(logData)
+        const log = new Training(logData)
+        console.log(log);
+        await log.save()
+    } catch (e) {
+        console.log("in catch")
+        throw new Error("Unable to create training log. Invalid data")
+    }
+}

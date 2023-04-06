7627c0afafed28234eb2ba900a63adfcb4958a28
weickman <122060261+weickman@users.noreply.github.com>
2023-04-06 19:20:03 -0400

    read users works
    hurray

--- server/mongodb/actions/readUsers.js
+++ server/mongodb/actions/readUsers.js
@@ -1,14 +1,17 @@
-import connectDB from "../index.js"
+import {connectDB, closeDB} from "../../utils/db"
 import User from "../models/user"
 
 
 const readUsers = async () => {
     await connectDB()
     try {
-        const users = await User.find({})
+        const users = await User.find({}, "_id firstName lastName")
+        console.log(users)
         return users
     } catch(e) { 
         console.log(e)
     }
 }
+closeDB()
 export {readUsers}
+
--- src/pages/api/admin/users.js
+++ src/pages/api/admin/users.js
@@ -2,7 +2,8 @@ import { readUsers } from "../../../../server/mongodb/actions/readUsers"
 
 export default async function handler(req, res) {
     try {
-        return res.status(200).json(readUsers())
+        let users = await readUsers()
+        return res.status(200).json(users)
     } catch(e) {
         return res.status(500).json({success: false, message: e.message})
     }

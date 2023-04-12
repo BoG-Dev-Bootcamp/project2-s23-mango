71cda993891e0bc3d277499cc2f62e10aeb33f24
weickman <122060261+weickman@users.noreply.github.com>
2023-04-12 17:05:18 -0400

    Update createUser.js

--- server/mongodb/actions/createUser.js
+++ server/mongodb/actions/createUser.js
@@ -3,12 +3,18 @@ import User from "../models/user"
 import bcrypt from "bcryptjs"
 
 export default async function createUser(userData) {
-    await connectDB()
     try {
         console.log("Inside of try")
-        const user = new User(userData)
+        const { firstName, lastName, email, password } = userData
+
+        const salt = await bcrypt.genSalt(10)
+        const hash = await bcrypt.hash(password, salt)
+
+        await connectDB()
+        const user = new User({firstName, lastName, email, password})
         console.log(user)
         await user.save()
+
     } catch (e) {
         console.log("In catch")
         throw new Error("Unable to create user. Invalid data")

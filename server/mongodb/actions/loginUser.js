58ce6a62d02e524437243d5fa11d8005ab92b2e8
rachelyuh <heavenyuh@gmail.com>
2023-04-12 22:46:48 -0400

    fixed login

--- server/mongodb/actions/loginUser.js
+++ server/mongodb/actions/loginUser.js
@@ -1,25 +1,31 @@
 import {connectDB, closeDB} from "../../utils/db"
 import User from "../models/user"
+import bcrypt from "bcryptjs"
 
 export default async function loginUser(userData) {
     await connectDB()
+    let isLogin = false;
     try {
         const password = userData.password
         const userEmail = {email: userData.email}
+
         const attemp = await User.findOne(userEmail)
-        const users = await User.count(userData)
+
         await closeDB()
-        if (users > 0) {
-            if (attemp.password == password) {
-                return true
-            }
-        } else {
-            return false
-        }
-    
-        
+        console.log(userData.password)
+        console.log(attemp.password)
+        await bcrypt.compare(userData.password, attemp.password, function(err, result) {
+            if (result) {
+                console.log("first true")
+                isLogin = true
+           } 
+        }); 
+        console.log("second false")
     } catch (e) {
         console.log(e)
         throw new Error("Unable to login user. Invalid data")
     }
+    console.log(isLogin)
+    return isLogin
+
 }
--- src/pages/api/user/login.js
+++ src/pages/api/user/login.js
@@ -1,17 +1,32 @@
-import loginUser from "../../../../server/mongodb/actions/loginUser"
 import checkMethod from "../../../../server/utils/checkMethod"
+import {connectDB, closeDB} from "../../../../server/utils/db"
+import User from "../../../../server/mongodb/models/user"
+import bcrypt from "bcryptjs"
 
 export default async function handler(req, res) {
+    checkMethod(["POST"], req.method)
+    await connectDB()
     try {
-        checkMethod(["POST"], req.method)
-        let isLogin = loginUser(req.body)
-        if (isLogin) {
+        let userData = req.body
+        const password = userData.password
+        const userEmail = {email: userData.email}
+
+        const attemp = await User.findOne(userEmail)
+
+        await closeDB()
+        console.log(userData.password)
+        console.log(attemp.password)
+        bcrypt.compare(userData.password, attemp.password, function(err, result) {
+            if (result) {
             return res.status(200).json({success: true, message: "Successfully logged user in"})
-        } else {
+           } else {
             return res.status(403).json({success: false, message: "User information invalid"})
         }
-    } catch(e) {
+        }); 
+        console.log("second false")
+    } catch (e) {
+        console.log(e)
         return res.status(500).json({success: false, message: e.message})
     }
-
+   
 }
